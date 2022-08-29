473e6576b184d8d0f3b2592d28942e7ed45eafdb
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-08-29 18:01:01 -0300

    edited some things

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
