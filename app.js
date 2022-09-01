22bff7f9bc738d6f09fcff59a60e441d6dde9dab
Rebelzob <karimdavid07@gmail.com>
2022-09-01 15:55:56 -0500

    footer n styles

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const methodOverride = require('method-override');  
 
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, './src/views'));
