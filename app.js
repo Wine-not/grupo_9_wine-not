11c05394c90a93f6df7dd7760a09b011cb4f77e7
Rebelzob <karimdavid07@gmail.com>
2022-09-05 12:04:53 -0500

    routes and controllers

--- app.js
+++ app.js
@@ -2,27 +2,22 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 // const methodOverride = require('method-override');
-
+const mainRouter = require('./src/routes/mainRouter');
+const productRouter = require('./src/routes/productRoutes');
 const app = express();
+
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 // app.use(methodOverride('_method'));
+//app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, './src/views'));
 
+app.use('/', mainRouter);
+app.use('/products', productRouter);
+
 app.listen(PORT, () => console.log(`[server] running at port ${PORT}`));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
-app.get('/productCart', (req, res) => {
-  res.render('./products/productCart');
-});
-app.get('/register', (req, res) => {
-  res.render('./users/register');
-});
-app.get('/login', (req, res) => {
-  res.render('./users/login');
-});
+
---
+++ src/controllers/productController.js
@@ -0,0 +1,10 @@
+const productController = {
+    productCart: (req, res) => {
+        res.render('./products/productCart');
+    },
+    productDetail: (req, res) => {
+        res.render('./products/productDetail');
+    },
+}
+
+module.exports = productController;
