73d7615570e9475fbef6e08a0402110ae2da2181
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-09-05 22:53:32 -0300

    added checks in register

--- public/javascripts/formValidation.js
+++ public/javascripts/formValidation.js
@@ -1,16 +1,19 @@
-const userName = document.querySelector('#name');
-const surname = document.querySelector('#surname');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const passwordRepeat = document.querySelector('#passwordRepeat');
-const birthDate = document.querySelector('#birthdate');
-const terms = document.querySelector('#terms');
-const form = document.querySelector('#form');
+const e = require('express');
+
+const nameIn = document.querySelector('#name');
+const surnameIn = document.querySelector('#surname');
+const emailIn = document.querySelector('#email');
+const passwordIn = document.querySelector('#password');
+const passwordRepeatIn = document.querySelector('#passwordRepeat');
+const birthDateIn = document.querySelector('#birthdate');
+const termsIn = document.querySelector('#terms');
+const formIn = document.querySelector('#form');
 
 const isRequired = (value) => (value === '' ? false : true);
 
-const isBetween = (length, min, max) =>
+const isBetween = (length, min, max) => {
   length < min || length > max ? false : true;
+};
 
 const isEmailValid = (email) => {
   const re =
@@ -26,6 +29,19 @@ const isPasswordSecure = (password) => {
 };
 
 const olderThan18 = (age) => (age < 18 ? false : true);
+
+const getAge = (dateInput) => {
+  let today = new Date();
+  let birth = new Date(dateInput);
+  let age = today.getFullYear() - birth.getFullYear();
+  let month = today.getMonth() - birth.getMonth();
+
+  if (month < 0 || (month === 0 && today.getDate() < birth.getDate())) {
+    age--;
+  }
+
+  return age;
+};
 
 const showError = (input, message) => {
   const formField = input.parentElement;
@@ -47,21 +63,99 @@ const showSucces = (input) => {
 
 const checkName = () => {
   let valid = false;
-  const name = userName.value.trim();
+  const userName = nameIn.value.trim();
 
-  if (!isRequired(name)) {
-    showError(userName, 'Name cannot be blank.');
+  if (!isRequired(userName)) {
+    showError(nameIn, 'Name cannot be blank.');
   } else {
     valid = true;
   }
+
+  return valid;
 };
 
 const checkSurname = () => {
   let valid = false;
-  const surname = surname.value.trim();
+  const surname = surnameIn.value.trim();
 
   if (!isRequired(surname)) {
-    showError(surname, 'Name cannot be blank.');
+    showError(surnameIn, 'Name cannot be blank.');
+  } else {
+    valid = true;
+  }
+
+  return valid;
+};
+
+const checkEmail = () => {
+  let valid = false;
+  const email = emailIn.value.trim();
+
+  if (!isRequired(email)) {
+    showError(emailIn, 'Email cannot be blank');
+  } else if (!isEmailValid(email)) {
+    showError(emailIn, 'Email is not valid');
+  } else {
+    valid = true;
+  }
+
+  return valid;
+};
+
+const checkPassword = () => {
+  let valid = false;
+  const password = passwordIn.value.trim();
+
+  if (!isRequired(password)) {
+    showError(passwordIn, 'Password cannot be blank');
+  } else if (!isPasswordSecure(password)) {
+    showError(
+      passwordIn,
+      'Password must has at least 8 characters that include at least 1 lowercase character, 1 uppercase characters and 1 number.'
+    );
+  } else {
+    valid = true;
+  }
+
+  return valid;
+};
+
+const checkConfirmPassword = () => {
+  let valid = false;
+  const passwordRepeat = passwordRepeatIn.value.trim();
+  const password = passwordIn.value.trim();
+
+  if (!isRequired(passwordRepeat)) {
+    showError(passwordRepeatIn, 'Password cannot be blank');
+  } else if (password !== passwordRepeat) {
+    showError(passwordRepeatIn, 'Passwords do not match');
+  } else {
+    valid = true;
+  }
+
+  return valid;
+};
+
+const checkAge = () => {
+  let valid = false;
+  const birth = birthDateIn.value;
+
+  if (!isRequired(birth)) {
+    showError(birthDateIn, 'Birthdate cannot be blank');
+  } else if (getAge(birth) < 18) {
+    showError(birthDateIn, 'You must be at least 18 to register');
+  } else {
+    valid = true;
+  }
+
+  return valid;
+};
+
+const checkTerms = () => {
+  let valid = false;
+
+  if (!termsIn.checked) {
+    showError(termsIn, 'You must accept our terms and condition');
   } else {
     valid = true;
   }
@@ -69,4 +163,24 @@ const checkSurname = () => {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  let isNameValid = checkName(),
+    isSurname = checkSurname(),
+    isEmailValid = checkEmail(),
+    isPasswordValid = checkPassword(),
+    isPasswordRepeatValid = checkConfirmPassword(),
+    isAgeValid = checkAge(),
+    areTermsAccepted = checkTerms();
+
+  let isFormValid =
+    isNameValid &&
+    isSurname &&
+    isEmailValid &&
+    isPasswordValid &&
+    isPasswordRepeatValid &&
+    isAgeValid &&
+    areTermsAccepted;
+
+  if (isFormValid) {
+  }
 });
