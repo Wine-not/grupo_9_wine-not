dc8912558ee3194716bddd82ff6baa043c275eb5
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-09-08 22:48:53 -0300

    finished productDetail

--- public/javascripts/utilities.js
+++ public/javascripts/utilities.js
@@ -1,4 +1,4 @@
-// Menu mobile
+// MOBILE MENU
 const openMark = document.querySelector('.navbar__menu-icon');
 const closeMark = document.querySelector('.mobile-menu__close');
 const nav = document.querySelector('.mobile-menu');
@@ -9,4 +9,31 @@ openMark.addEventListener('click', () => {
 
 closeMark.addEventListener('click', () => {
   nav.style.width = '0%';
+});
+
+// FORM INPUT PRODUCT DETAIL
+
+const buttons = [...document.querySelectorAll('.qty__button')];
+let newValue;
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let oldValue = Number(
+      buttons[0].parentElement.querySelector('input').value
+    );
+
+    if (button.textContent == '+') {
+      newValue = oldValue + 1;
+    }
+
+    if (button.textContent == '-') {
+      if (oldValue > 0) {
+        newValue = oldValue - 1;
+      } else {
+        newValue = 0;
+      }
+    }
+
+    buttons[0].parentElement.querySelector('input').value = newValue;
+  });
 });
