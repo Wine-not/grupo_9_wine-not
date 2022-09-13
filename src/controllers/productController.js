dc4fe31a7feeac4957f6cb6da53cf03e687fc4eb
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-09-13 14:37:33 -0300

    started product create controller

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,13 +1,30 @@
-const productController = {
-    productCart: (req, res) => {
-        res.render('./products/productCart');
-    },
-    productDetail: (req, res) => {
-        res.render('./products/productDetail');
-    },
-    productCreate: (req, res) => {
-        res.render('./products/productCreate')
-    },
-}
+const fs = require('fs');
+const path = require('path');
+const { validationResult } = require('express-validator');
 
-module.exports = productController;
+module.exports = {
+  // Show product cart
+  productCart: (req, res) => {
+    res.render('./products/productCart');
+  },
+
+  // Show one product
+  productDetail: (req, res) => {
+    res.render('./products/productDetail');
+  },
+
+  // Show product create form
+  productCreate: (req, res) => {
+    res.render('./products/productCreate');
+  },
+
+  // Process product create form
+  productCreateProcess: (req, res, next) => {
+    const file = req.file;
+    if (!file) {
+      const error = new Error('Please select an image file');
+      error.httpStatusCode = 400;
+      return next(error);
+    }
+  },
+};
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,10 +1,72 @@
 const express = require('express');
 const router = express.Router();
-
+const path = require('path');
+const multer = require('multer');
 const productController = require('../controllers/productController');
+const { check } = require('express-validator');
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/images/products');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);
+  },
+});
+
+const uploadImage = multer(storage);
+
+let validation = [
+  check('name')
+    .notEmpty()
+    .withMessage('Enter the product name')
+    .bail()
+    .isLength({ min: 5, max: 35 })
+    .withMessage('Name must be between 5 and 35 characters'),
+  check('price')
+    .notEmpty()
+    .withMessage('Enter a price for the product')
+    .bail()
+    .isNumeric()
+    .withMessage('Price must be a number'),
+  check('brand')
+    .notEmpty()
+    .withMessage('Enter a brand name')
+    .bail()
+    .isLength({ min: 5, max: 35 })
+    .withMessage('Brand must be between 5 and 35 characters'),
+  check('grape')
+    .notEmpty()
+    .withMessage("Enter product's grape")
+    .bail()
+    .isLength({ min: 5, max: 35 })
+    .withMessage('Grape must be between 5 and 35 characters'),
+  check('rating')
+    .notEmpty()
+    .withMessage('Enter product rating')
+    .bail()
+    .isNumeric()
+    .withMessage('Product rating must be a number'),
+  check('region')
+    .notEmpty()
+    .withMessage("Enter product's region")
+    .bail()
+    .isLength({ min: 5, max: 35 })
+    .withMessage('Region must be between 5 and 35 characters'),
+];
+
+// Product cart
 router.get('/productCart', productController.productCart);
+
+// Show products
 router.get('/productDetail', productController.productDetail);
+
+// Create products
 router.get('/productCreate', productController.productCreate);
+router.post(
+  '/',
+  uploadImage.single('image'),
+  productController.productCreateProcess
+);
 
 module.exports = router;
