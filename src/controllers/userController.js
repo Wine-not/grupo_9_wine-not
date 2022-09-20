ce00968a8a755843e7fba01e04e4e04bde0b68a9
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-09-20 13:15:53 -0300

    saved changes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,48 +10,48 @@ const userController = {
     res.render('./users/login');
   },
 
-  // loginProcess: (req, res) => {
-  //   let errors = validationResult(req);
-  //   if (errors.isEmpty()) {
-  //     let userToLogin = users.find((user) => user.email == req.body.email);
-  //     if (userToLogin) {
-  //       let isOkThePassword = bcrypt.compareSync(
-  //         req.body.password,
-  //         userToLogin.password
-  //       );
-  //       if (isOkThePassword) {
-  //         delete userToLogin.password;
-  //         req.session.userLogged = userToLogin;
-  //         if (req.body.remember_user) {
-  //           res.cookie('userEmail', req.body.email, { maxAge: 1000 * 60 * 60 });
-  //         }
-  //         return res.redirect('/users/profile');
-  //       }
-  //       return res.render('./users/login', {
-  //         errors: {
-  //           email: {
-  //             msg: 'Incorrect password',
-  //           },
-  //         },
-  //       });
-  //     }
-  //     return res.render('./users/login', {
-  //       errors: {
-  //         email: {
-  //           msg: 'You are not registered',
-  //         },
-  //       },
-  //     });
-  //   }
-  //   return res.render('./users/login', {
-  //     errors: errors.mapped(),
-  //     old: req.body,
-  //   });
-  // },
+  loginProcess: (req, res) => {
+    let errors = validationResult(req);
+    if (errors.isEmpty()) {
+      let userToLogin = users.find((user) => user.email == req.body.email);
+      if (userToLogin) {
+        let isOkThePassword = bcrypt.compareSync(
+          req.body.password,
+          userToLogin.password
+        );
+        if (isOkThePassword) {
+          delete userToLogin.password;
+          req.session.userLogged = userToLogin;
+          if (req.body.remember_user) {
+            res.cookie('userEmail', req.body.email, { maxAge: 1000 * 60 * 60 });
+          }
+          return res.redirect('/users/profile');
+        }
+        return res.render('./users/login', {
+          errors: {
+            email: {
+              msg: 'Incorrect password',
+            },
+          },
+        });
+      }
+      return res.render('./users/login', {
+        errors: {
+          email: {
+            msg: 'You are not registered',
+          },
+        },
+      });
+    }
+    return res.render('./users/login', {
+      errors: errors.mapped(),
+      old: req.body,
+    });
+  },
 
-  // profile: (req, res) => {
-  //   res.render('./users/profile');
-  // },
+  profile: (req, res) => {
+    res.render('./users/profile');
+  },
 
   register: (req, res) => {
     res.render('./users/register');
@@ -82,11 +82,10 @@ const userController = {
     let errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      // console.log(errors.mapped());
-      // res.render('./users/register', {
-      //   errors: errors.mapped(),
-      //   old: req.body,
-      // });
+      res.render('./users/register', {
+        errors: errors.mapped(),
+        old: req.body,
+      });
     }
 
     let newUser = {
@@ -112,40 +111,40 @@ const userController = {
     res.redirect('/');
   },
 
-  // edit: (req, res) => {
-  //   let userId = req.params.idUser;
-  //   let userToEdit = users.find((user) => user.userId == userId);
-  //   res.render('./users/edit', { userToEdit: userToEdit });
-  // },
+  edit: (req, res) => {
+    let userId = req.params.idUser;
+    let userToEdit = users.find((user) => user.userId == userId);
+    res.render('./users/edit', { userToEdit: userToEdit });
+  },
 
-  // update: (req, res) => {
-  //   let userId = req.params.userId;
-  //   let userToEdit = users.find((user) => user.userId == userId);
+  update: (req, res) => {
+    let userId = req.params.userId;
+    let userToEdit = users.find((user) => user.userId == userId);
 
-  //   userToEdit = {
-  //     userId: req.body.nickname,
-  //     ...req.body,
-  //   };
+    userToEdit = {
+      userId: req.body.nickname,
+      ...req.body,
+    };
 
-  //   let usersUpdated = users.map((user) => {
-  //     if (user.userId == userToEdit.userId) {
-  //       return (user = { ...userToEdit });
-  //     }
-  //     return user;
-  //   });
+    let usersUpdated = users.map((user) => {
+      if (user.userId == userToEdit.userId) {
+        return (user = { ...userToEdit });
+      }
+      return user;
+    });
 
-  //   fs.writeFileSync(usersFilePath, JSON.stringify(usersUpdated, null, ' '));
-  //   users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-  //   res.redirect('./users/profile');
-  // },
+    fs.writeFileSync(usersFilePath, JSON.stringify(usersUpdated, null, ' '));
+    users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+    res.redirect('./users/profile');
+  },
 
-  // delete: (req, res) => {
-  //   let userId = req.params.userId;
-  //   let finalUsers = users.filter((user) => user.userId != userId);
-  //   console.log(finalUsers);
-  //   fs.writeFileSync(usersFilePath, JSON.stringify(finalUsers, null, ' '));
-  //   res.redirect('/');
-  // },
+  delete: (req, res) => {
+    let userId = req.params.userId;
+    let finalUsers = users.filter((user) => user.userId != userId);
+    console.log(finalUsers);
+    fs.writeFileSync(usersFilePath, JSON.stringify(finalUsers, null, ' '));
+    res.redirect('/');
+  },
 };
 
 module.exports = userController;
