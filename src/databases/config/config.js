2e539a68f7d3618a1ed640d92d544680366d1155
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-11-03 22:03:46 -0300

    set up db

--- src/databases/config/config.js
+++ src/databases/config/config.js
@@ -1,69 +1,26 @@
-require('dotenv').config();
-const env = process.env;
-
-const development = {
-  username: env.DATABASE_USER,
-  password: env.DATABASE_PASS,
-  database: env.DATABASE_NAME,
-  host: env.DATABASE_HOST,
-  dialect: env.DATABASE_DIALECT,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true,
-    },
+module.exports = {
+  'development': {
+    'username': 'root',
+    'password': null,
+    'database': 'winenot_db',
+    'host': '127.0.0.1',
+    'dialect': 'mysql',
+    'operatorAliases': false,
   },
-  define: {
-    timestamps: false,
+  'test': {
+    'username': 'root',
+    'password': null,
+    'database': 'winenot_db',
+    'host': '127.0.0.1',
+    'dialect': 'mysql',
+    'operatorAliases': false,
   },
-};
-
-const production = {
-  username: env.DATABASE_USER,
-  password: env.DATABASE_PASS,
-  database: env.DATABASE_NAME,
-  host: env.DATABASE_HOST,
-  dialect: env.DATABASE_DIALECT,
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true,
-    },
-  },
-  define: {
-    timestamps: false,
-  },
-};
-
-const test = {
-  username: env.DATABASE_USER,
-  password: env.DATABASE_PASS,
-  database: env.DATABASE_NAME_TEST,
-  host: env.DATABASE_HOST,
-  dialect: env.DATABASE_DIALECT,
-};
-
-module.exports = { development, production, test };
-
-/*module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_wine_not",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  'production': {
+    'username': 'root',
+    'password': null,
+    'database': 'winenot_db',
+    'host': '127.0.0.1',
+    'dialect': 'mysql',
+    'operatorAliases': false,
   }
 }
-*/
