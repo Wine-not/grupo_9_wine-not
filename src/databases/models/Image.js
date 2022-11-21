6506b53d4d2aa6576484ecd1d3b5312928477703
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-11-21 20:42:38 -0300

    showing images in index

--- src/databases/models/Image.js
+++ src/databases/models/Image.js
@@ -1,34 +1,34 @@
 module.exports = (sequelize, dataTypes) => {
   let alias = 'Image';
-  
+
   let cols = {
     id: {
       type: dataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
-      allowNull: false
+      allowNull: false,
     },
     path: {
       type: dataTypes.STRING(250),
       allowNull: false,
-      defaultValue: 'product_default.jpg'
-    }
+      defaultValue: 'product_default.jpg',
+    },
   };
-  
+
   let config = {
     tableName: 'images',
     timestamps: false,
   };
-  
+
   const Image = sequelize.define(alias, cols, config);
 
   // Associations
   Image.associate = function (models) {
     Image.hasOne(models.Product, {
-      as: "product_image",
-      foreignKey: "image_id"
+      // as: 'product_image',
+      foreignKey: 'image_id',
     });
   };
-  
+
   return Image;
-}
+};
