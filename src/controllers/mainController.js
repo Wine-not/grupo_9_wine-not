b00eb67a23c6b919835802c66da98677c71a4021
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-11-21 19:27:43 -0300

    index db query, images not done

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,25 +1,28 @@
-const fs = require('fs');
-const path = require('path');
-// const productsFilePath = path.join(__dirname, '../data/products.json');
-// const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
-// fs.readFile(productsFilePath, (err, productData) => {
-//   if (err) throw err;
-//   products = JSON.parse(productData);
-// });
-
-// const selectedProducts = products.filter((product) => {
-//   return product.isSelection === true;
-// });
-//
-// const saleProducts = products.filter((product) => {
-//   return product.inSale === true;
-// });
+const db = require('../databases/models');
+const { Op } = require('sequelize');
+const sequelize = db.sequelize;
 
 const controller = {
-  index: (req, res) => {
-    // res.render('index', { selectedProducts, saleProducts });
-    res.render('index');
+  index: async (req, res) => {
+    let selectedProducts = await db.Product.findAll({
+      where: {
+        is_selection: {
+          [Op.eq]: true,
+        },
+      },
+      limit: 4,
+    });
+
+    let saleProducts = await db.Product.findAll({
+      where: {
+        in_sale: {
+          [Op.eq]: true,
+        },
+      },
+      limit: 4,
+    });
+
+    res.render('index', { selectedProducts, saleProducts });
   },
 };
 
