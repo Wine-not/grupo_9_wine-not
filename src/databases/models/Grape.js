116983b3f8840be66e531df7fa92b60057972505
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-11-24 19:11:11 -0300

    updated merge

--- src/databases/models/Grape.js
+++ src/databases/models/Grape.js
@@ -1,40 +1,5 @@
 module.exports = (sequelize, dataTypes) => {
-<<<<<<< HEAD
-    let alias = 'Grape';
-    let cols = {
-        id: {
-            type: dataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            autoIncrement: true,
-        },
-        grape_name: {
-            type: dataTypes.STRING(100),
-            allowNull: false,
-        },
-        grape_type: {
-            type: dataTypes.STRING(100),
-            allowNull: false, 
-        },
-        created_at: {
-            type: dataTypes.DATE
-         },
-        updated_at: {
-            type: dataTypes.DATE
-        },
-    };
-    let config = {
-        timestamps: true,
-        createdAt: "created_at",
-        updatedAt: "updated_at",
-        deletedAt: false,
-    };
-    
-        
-    const Grape = sequelize.define(alias, cols, config);
-=======
   let alias = 'Grape';
->>>>>>> 7d971471084032a5b3c82b3942e082271df9fb74
 
   let cols = {
     id: {
