53c3a1333b84df5c073fff596b0cab017f55604a
JuanCruzEusebi <juancruzeusebi@gmail.com>
2022-11-25 22:43:14 -0300

    working on login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
+// const bcryptjs = require('bcryptjs')
 const { validationResult } = require('express-validator');
 const { validateLogin } = require('../utilities/validateLogin');
 const validationsLogin = require('../routes/userRouter');
@@ -13,37 +14,36 @@ module.exports = {
     res.render('./users/login');
   },
 
-  // Process login form
   loginProcess: (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-          return res.status(400).json({
-              success: false,
-              errors: errors.array()
-          });
-      } else {
-        db.User.findAll()
-        .then((users) => {
-        for (let user of users) {       
-          if (req.body.email == user.email) {
-            let userToLogin = user;
-            bcrypt.compare(req.body.password, userToLogin.password, function(err, result) {
-              if (result == false) {
-               req.session.loggedUser = userToLogin;               
-                res.render('./users/profile', { userToLogin });
-              } else {
-                 res.json('error')
-              }
-            })
-          }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            success: false,
+            errors: errors.array()
+        });
+    } else {
+      db.User.findAll()
+      .then((users) => {
+      for (let user of users) {       
+        if (req.body.email == user.email) {
+          let userToLogin = user;
+          bcrypt.compare(req.body.password, userToLogin.password, function(err, result) {
+            if (result == false) {
+             req.session.loggedUser = userToLogin;               
+              res.render('./users/profile', { userToLogin });
+            } else {
+               res.json('error')
+            }
+          })
         }
-      }) 
-     }  
-  },
+      }
+    }) 
+   }  
+},
 
   // Shows user profile
   profile: (req, res) => {    
-    res.render('./users/profile');
+    res.render('./users/profile', {userToLogin: userToLogin});
   },
 
   // Shows register form
@@ -66,7 +66,7 @@ module.exports = {
       name: req.body.name,
       surname: req.body.lastName,
       email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 5),
+      password: bcrypt.hash(req.body.password, 5),
       birthdate: req.body.birthdate,
       role_id: 1,
       address_id: 1
@@ -76,42 +76,33 @@ module.exports = {
 
   // Shows user to edit
   edit: (req, res) => {    
-    if (res.session.loggedUser == undefined) {
-      res.json('no estas logeado')
-    } else {
-      res.json('estas logeado')
-    }
+    db.User.findByPk(23)
+      .then((userToEdit) => {
+        res.render('../views/users/edit.ejs', { userToEdit });
+    })   
+    
   },
 
   // Edit the user
   update: (req, res) => {
-    // let userId = req.params.userId;
-    // let userToEdit = users.find((user) => user.userId == userId);
-
-    // userToEdit = {
-    //   userId: req.body.nickname,
-    //   ...req.body,
-    // };
-
-    // let usersUpdated = users.map((user) => {
-      // if (user.userId == userToEdit.userId) {
-      //   return (user = { ...userToEdit });
-      // }
-      // return user;
-    // });
-
-    // fs.writeFileSync(usersFilePath, JSON.stringify(usersUpdated, null, ' '));
-    // users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
-    // res.redirect('./users/profile');
+    db.User.update({
+      name: req.body.name,
+      surname: req.body.lastName,
+      email: req.body.email,
+    }, {
+      where: {
+        id: 23
+      }
+    })
+    res.render('./');
   },
 
   // Delete the user
   delete: (req, res) => {
-    // let userId = req.params.userId;
-    // let finalUsers = users.filter((user) => user.userId != userId);
-    // console.log(finalUsers);
-    // fs.writeFileSync(usersFilePath, JSON.stringify(finalUsers, null, ' '));
-    // res.redirect('/');
+    db.User.destroy({
+      where: {id: 23}
+    })
+    res.json('deleted')
   },
 };
 
