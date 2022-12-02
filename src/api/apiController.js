3089ed507a17c6768f8b95fad573d73b38555352
JoaquinGuzman <49413000+joacoguzmanz@users.noreply.github.com>
2022-12-01 23:21:10 -0300

    advanced users and prods apis

--- src/api/apiController.js
+++ src/api/apiController.js
@@ -7,6 +7,7 @@ module.exports = {
       attributes: ['id', 'name', 'email']
     });
   
+    // TODO detail endpoint
     res.status(200).json({
       count: users.length,
       data: users,
@@ -15,11 +16,35 @@ module.exports = {
   },
   
   detailUser: async (req, res) => {
-  
+    let user = await db.User.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'name', 'surname', 'email', 'birthdate']
+    });
+    
+    res.status(200).json({
+      data: user,
+      status: 200,
+      detail: `http://localhost:3000/api/users/${user.id}`
+    });
   },
   
   listProducts: async (req, res) => {
-  
+    let products = await db.Product.findAll({
+      include: [
+        {
+          model: db.Grape
+        }
+      ]
+    })
+    
+    // TODO object con una propiedad por categoria
+    // TODO detail endpoint
+    res.status(200).json({
+      count: products.length,
+      data: products,
+    })
   },
   
   detailProduct: async (req, res) => {
