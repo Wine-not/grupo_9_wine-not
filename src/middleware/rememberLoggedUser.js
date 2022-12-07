9232c92979f2c275598f0b07b3721a4e196d4f49
JuanCruzEusebi <juancruzeusebi@gmail.com>
2022-12-07 16:51:12 -0300

    login cookies

--- src/middleware/rememberLoggedUser.js
+++ src/middleware/rememberLoggedUser.js
@@ -1,26 +1,15 @@
 const db = require('../databases/models');
 
 
-<<<<<<< HEAD
 module.exports = rememberLoggedUser = async (req, res, next) => {
   if (req.cookies.rememberMe != undefined && req.session.loggedUser == undefined) {
     let userToLogin = await db.User.findOne({
-=======
-
-module.exports = rememberMe =  (req, res, next) => {
-  if (req.cookies.rememberMe != undefined && req.session.loggedUser == undefined) {
-    let userToLogin =  db.User.findOne({
->>>>>>> ddf2a531b67cb56652edb15a71d3295ca7bf75ef
       where: {
         email: req.cookies.rememberMe,
       }
     });
     req.session.loggedUser = userToLogin;
   }
-<<<<<<< HEAD
   next();
-=======
-  next()
->>>>>>> ddf2a531b67cb56652edb15a71d3295ca7bf75ef
 }
 
